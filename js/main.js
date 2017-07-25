501a6a65220bc4e250ab90ba8c7ff2acb7a8de96
Leila.L <leila.lababsa@hotmail.fr>
2017-07-25 10:38:54 +0200

    modified html+css

--- js/main.js
+++ js/main.js
@@ -7,3 +7,22 @@
 // var yyyy = today.getFullYear();
 // if(dd<10){dd='0'+dd} if(mm<10){mm='0'+mm} today = mm+'/'+dd+'/'+yyyy;
 //       });
+
+var answers = ["oui", "je ne sais pas", "non"];
+var message;
+
+
+$("button").click(function(){
+      var message =  $("input:text").val();
+      document.getElementByClassName("chat-box").innerHTML+="<div>"
+    });
+
+
+    $("input").keypress(function (x) {
+      if (x.which == 13) {
+        setTimeout(function(){
+          $("user").append($("input").val())
+          $("input").val("");}, 200);
+        popo()
+      }
+    })
