a9b2c942367c79853d80916c72c33179b2a22e1d
Leila.L <leila.lababsa@hotmail.fr>
2017-07-25 11:49:17 +0200

    check indentation

--- js/main.js
+++ js/main.js
@@ -1,27 +1,26 @@
-
 var answers = ["oui", "je ne sais pas", "non", "pourquoi", "ah bon"];
 var message;
 var index = -1;
 
 $(document).keypress(function(touche) {
   var entrer = touche.which
-  if ( entrer == 13) {
-   chat();
+  if (entrer == 13) {
+    chat();
   }
 });
 
-$("#btn1").click(function(){
+$("#btn1").click(function() {
   chat();
 })
 
 function chat() {
-index++
-      var message =  $("#textArea").val();
-      $(".chat-box").append("<div class='message-box'><div class='picture'><img src='img/mask.jpg' alt='anonymous'/></div><div class='message'><span>Me</span><p>" + message + "</p></div></div>");
+  index++
+  var message = $("#textArea").val();
+  $(".chat-box").append("<div class='message-box'><div class='picture'><img src='img/mask.jpg' alt='anonymous'/></div><div class='message'><span>Me</span><p>" + message + "</p></div></div>");
 
 
-setTimeout(function() {
-  $(".chat-box").append("<div class='message-box left-img'><div class='picture'><img src='img/images.jpeg' alt='dewey'/></div><div class='message'><span>Him</span><p>" + answers[index] + "</p></div></div>");
-}, 2000);
+  setTimeout(function() {
+    $(".chat-box").append("<div class='message-box left-img'><div class='picture'><img src='img/images.jpeg' alt='dewey'/></div><div class='message'><span>Him</span><p>" + answers[index] + "</p></div></div>");
+  }, 2000);
 
 }
